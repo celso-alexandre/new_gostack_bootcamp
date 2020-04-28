82fafefb6d0aca4d51a380ee17a1c28e8fd4364a
Celso Alexandre <celsoalexandre@live.com>
2020-04-27 21:31:35 -0300

    Starting our web app frontend with React.js and TypeScript: #1 Structuring pages: Done

--- gobarber/web/src/pages/SignIn/index.tsx
+++ gobarber/web/src/pages/SignIn/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
@@ -7,16 +11,38 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail is mandatory')
+          .email('Please type a valid e-mail'),
+        password: Yup.string().required('Password is mandatory'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
     <Container>
       <Content>
         <img src={logoImg} alt="logo" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Login</h1>
 
-          <Input icon={FiMail} name="email" type="email" placeholder="E-mail" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
           <Input
             icon={FiLock}
             name="password"
@@ -28,7 +54,7 @@ const SignIn: React.FC = () => (
           </Button>
 
           <a href="forgot">Forgot my password</a>
-        </form>
+        </Form>
 
         <a href="c">
           <FiLogIn />
@@ -37,7 +63,7 @@ const SignIn: React.FC = () => (
       </Content>
       <Background />
     </Container>
-  </>
-);
+  );
+};
 
 export default SignIn;
--- gobarber/web/src/pages/SignUp/index.tsx
+++ gobarber/web/src/pages/SignUp/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
@@ -9,9 +12,28 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is mandatory'),
+        email: Yup.string()
+          .required('E-mail is mandatory')
+          .email('Please type a valid e-mail'),
+        password: Yup.string().min(6, 'Password is too short'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <>
@@ -20,16 +42,11 @@ const SignUp: React.FC = () => {
         <Content>
           <img src={logoImg} alt="logo" />
 
-          <Form onSubmit={handleSubmit}>
+          <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Registration</h1>
 
             <Input icon={FiUser} name="name" placeholder="Name" />
-            <Input
-              icon={FiMail}
-              name="email"
-              type="email"
-              placeholder="E-mail"
-            />
+            <Input icon={FiMail} name="email" placeholder="E-mail" />
             <Input
               icon={FiLock}
               name="password"
