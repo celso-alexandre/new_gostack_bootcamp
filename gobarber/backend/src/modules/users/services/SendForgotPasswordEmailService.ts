6399a06352a36fe46cc264dd904391809f3fa23d
Celso Alexandre <celsoalexandre@live.com>
2020-05-11 21:16:25 -0300

    Continuing app backend: #2 Tests refactoring

--- gobarber/backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ gobarber/backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -7,6 +7,7 @@ import IMailProvider from '@shared/container/providers/MailProvider/models/IMail
 import IUserTokensRepository from '../repositories/IUserTokensRepository'
 
 import AppError from '@shared/errors/AppError'
+import path from 'path'
 
 interface IRequest {
   email: string
@@ -34,6 +35,13 @@ export default class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id)
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs'
+    )
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -41,10 +49,10 @@ export default class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Password recovery',
       templateData: {
-        template: 'Hello, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     })
--- gobarber/backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ gobarber/backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -2,10 +2,7 @@ import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO'
 import IMailTemplateProvider from '../models/IMailTemplateProvider'
 
 export default class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({
-    template,
-    variables,
-  }: IParseMailTemplateDTO): Promise<string> {
-    return template
+  public async parse(): Promise<string> {
+    return 'Mail content'
   }
 }
