85f1aced8fa90da14f13f6cb222bfb4fa52bda75
Celso Alexandre <celsoalexandre@live.com>
2020-05-17 17:17:41 -0300

    Finishing app back-end: #3: Cache and security - Invalidating cache

--- gobarber/backend/src/modules/users/services/CreateUserService.ts
+++ gobarber/backend/src/modules/users/services/CreateUserService.ts
@@ -5,6 +5,8 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider'
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
+
 import AppError from '@shared/errors/AppError'
 
 interface IRequest {
@@ -20,7 +22,10 @@ export default class CreateUserService {
     private usersRepository: IUsersRepository,
 
     @inject('HashProvider')
-    private hashProvider: IHashProvider
+    private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -37,6 +42,8 @@ export default class CreateUserService {
       email,
       password: hashedPassword,
     })
+
+    await this.cacheProvider.invalidateByPrefix('providers-list')
 
     return user
   }
