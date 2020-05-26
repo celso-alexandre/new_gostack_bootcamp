9d92dd8b0de959e5f739ea4d4f6195f51ca2f71f
Celso Alexandre <celsoalexandre@live.com>
2020-05-26 00:23:37 -0300

    Finalizing web app front-end: Dashboard - Calendar functionality: Load API days availability and use it in calendar

--- gobarber/web/src/hooks/auth.tsx
+++ gobarber/web/src/hooks/auth.tsx
@@ -32,6 +32,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -46,7 +48,9 @@ const AuthProvider: React.FC = ({ children }) => {
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
-    await setData({ token, user });
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
+    setData({ token, user });
   }, []);
 
   const signOut = useCallback(() => {
