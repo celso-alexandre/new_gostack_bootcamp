fc36eb4ff8b38def4820cfafa68988134e7c6e73
Celso Alexandre <celsoalexandre@live.com>
2020-05-27 22:55:58 -0300

    Finalizing web app front-end: User Profile - Avatar image update funcionality done

--- gobarber/web/src/pages/Profile/index.tsx
+++ gobarber/web/src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, ChangeEvent } from 'react';
 import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
@@ -27,7 +27,7 @@ const Profile: React.FC = () => {
   const { addToast } = useToast();
   const history = useHistory();
 
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SignUpFormData) => {
@@ -73,6 +73,26 @@ const Profile: React.FC = () => {
     [addToast, history]
   );
 
+  const handleAvatarChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const data = new FormData();
+
+      if (e.target.files) {
+        data.append('avatar', e.target.files[0]);
+
+        api.patch('/users/avatar', data).then((response) => {
+          updateUser(response.data);
+
+          addToast({
+            type: 'success',
+            title: 'Avatar atualizado!',
+          });
+        });
+      }
+    },
+    [addToast, updateUser]
+  );
+
   return (
     <>
       <Container>
@@ -95,9 +115,11 @@ const Profile: React.FC = () => {
           >
             <AvatarInput>
               <img src={user.avatar_url} alt={user.name} />
-              <button type="button">
+              <label htmlFor="avatar">
                 <FiCamera />
-              </button>
+
+                <input type="file" id="avatar" onChange={handleAvatarChange} />
+              </label>
             </AvatarInput>
 
             <h1>My profile</h1>
